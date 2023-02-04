0c45cf310300822514d78702d48ab266c16cf772
Burhan Qadri <burhan.qadri@alumni.ubc.ca>
2023-02-04 11:52:06 -0500

    error handler added, lets see

--- server.js
+++ server.js
@@ -43,6 +43,26 @@ async function startApolloServer() {
   await server.start();
   server.applyMiddleware({ app, path: "/graphql" });
 
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    if (err.status !== 404) {
+      console.error(err);
+    }
+
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: {},
+    });
+  });
+
   app.listen(PORT, () => {
     console.log("Running GraphQL server...");
   });
